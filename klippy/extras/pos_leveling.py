4c8f7d20ea8623c57291fd05abe90b05dabde7b3
Grzegorz Gajoch <gajoch@gmail.com>
2020-01-21 04:01:43 +0100

    --wip-- [skip ci]

--- klippy/extras/pos_leveling.py
+++ klippy/extras/pos_leveling.py
@@ -8,7 +8,7 @@ class PosLevel:
         self.gcode = self.printer.lookup_object('gcode')
         self.gcode.register_command('POS_LEVEL', self.cmd_POS_LEVEL,
                                     desc=self.cmd_POS_LEVEL_help)
-        self.gcode.register_command('MINX', self.MINX,
+        self.gcode.register_command('MINX', self.cmd_MINX,
                                     desc=self.cmd_POS_LEVEL_help)
 
     cmd_POS_LEVEL_help = "Setup config for only MIN/MAX bed positions"
@@ -16,7 +16,7 @@ class PosLevel:
     def cmd_POS_LEVEL(self, params):
         self.gcode.respond_info("Wooorks!")
 
-    def MINX(self, params):
+    def cmd_MINX(self, params):
         with open('/home/pi/test', 'w') as f:
             f.write('dupa')
         self.gcode.respond_info("MINX Woorks!")
